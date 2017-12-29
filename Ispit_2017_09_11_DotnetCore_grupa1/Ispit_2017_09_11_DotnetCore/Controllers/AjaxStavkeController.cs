58342edcd86fb87c5c6ed447a722563ed1555181
Adil Joldić <adil@edu.fit.ba>
2017-12-29 11:52:51 +0100

    ajax event

---
+++ Ispit_2017_09_11_DotnetCore_grupa1/Ispit_2017_09_11_DotnetCore/Controllers/AjaxStavkeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ispit_2017_09_11_DotnetCore.EF;
+using Ispit_2017_09_11_DotnetCore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ispit_2017_09_11_DotnetCore.Controllers
+{
+    public class AjaxStavkeController : Controller
+    {
+        private MojContext _context;
+        public AjaxStavkeController(MojContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index(int odjeljenjeID)
+        {
+            StavkeIndexVM model = new StavkeIndexVM();
+            model.Rows = _context.OdjeljenjeStavka
+                .Where(x => x.OdjeljenjeId == odjeljenjeID)
+                .Select(y => new StavkeIndexVM.Row
+                {
+                    BrojUDnevniku = y.BrojUDnevniku,
+                    BrojZakljucnihOcjena = _context.DodjeljenPredmet.Count(s => s.OdjeljenjeStavkaId ==y.Id),
+                    OdjeljenjeStavkaID = y.Id,
+                    Ucenik = y.Ucenik.ImePrezime
+                })
+                .ToList();
+            return PartialView(model);
+        }
+
+        public IActionResult Obrisi(int OdjeljenjeStavkaId)
+        {
+            var x = _context.OdjeljenjeStavka.Find(OdjeljenjeStavkaId);
+            _context.OdjeljenjeStavka.Remove(x);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Ispit_2017_09_11_DotnetCore_grupa1/Ispit_2017_09_11_DotnetCore/wwwroot/JFIT.js
+++ Ispit_2017_09_11_DotnetCore_grupa1/Ispit_2017_09_11_DotnetCore/wwwroot/JFIT.js
@@ -1,17 +1,21 @@
 ﻿
-$(document).ready(function () {
 
+function ajaxEventi() {
     $("button[ajax-poziv='da']").click(function (event) {
+        $(this).attr("ajax-poziv", "dodan");
+
         event.preventDefault();
         var urlZaPoziv = $(this).attr("ajax-url");
         var divZaRezultat = $(this).attr("ajax-rezultat");
 
         $.get(urlZaPoziv, function (data, status) {
             $("#" + divZaRezultat).html(data);
+            ajaxEventi();
         });
     });
 
     $("a[ajax-poziv='da']").click(function (event) {
+        $(this).attr("ajax-poziv", "dodan");
         event.preventDefault();
         var urlZaPoziv1 = $(this).attr("ajax-url");
         var urlZaPoziv2 = $(this).attr("href");
@@ -19,24 +23,26 @@ $(document).ready(function () {
 
         var urlZaPoziv;
 
-        if (typeof urlZaPoziv1 === 'string' || urlZaPoziv1 instanceof String)
+        if (urlZaPoziv1 instanceof String)
             urlZaPoziv = urlZaPoziv1;
         else
             urlZaPoziv = urlZaPoziv2;
 
         $.get(urlZaPoziv, function (data, status) {
             $("#" + divZaRezultat).html(data);
+            ajaxEventi();
         });
     });
 
     $("form[ajax-poziv='da']").submit(function (event) {
+        $(this).attr("ajax-poziv", "dodan");
         event.preventDefault();
         var urlZaPoziv1 = $(this).attr("ajax-url");
         var urlZaPoziv2 = $(this).attr("action");
         var divZaRezultat = $(this).attr("ajax-rezultat");
 
         var urlZaPoziv;
-        if (typeof urlZaPoziv1 === 'string' || urlZaPoziv1 instanceof String)
+        if (urlZaPoziv1 instanceof String)
             urlZaPoziv = urlZaPoziv1;
         else
             urlZaPoziv = urlZaPoziv2;
@@ -49,9 +55,14 @@ $(document).ready(function () {
             data: form.serialize(),
             success: function (data) {
                 $("#" + divZaRezultat).html(data);
+                ajaxEventi();
             }
         });
     });
+}
+$(document).ready(function () {
+
+    ajaxEventi();
 
 
 });
