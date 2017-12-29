de5b27c828ead7a368ab5dbb81f69870377bb1fa
Adil Joldić <adil@edu.fit.ba>
2017-12-29 09:17:14 +0100

    gr1

--- Ispit_2017_09_11_DotnetCore_grupa2/Ispit_2017_09_11_DotnetCore/Controllers/AjaxTestController.cs
+++ Ispit_2017_09_11_DotnetCore_grupa2/Ispit_2017_09_11_DotnetCore/Controllers/AjaxTestController.cs
@@ -6,13 +6,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ispit_2017_09_11_DotnetCore.Controllers
 {
-    public class OdjeljenjeController : Controller
+    public class AjaxTestController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
 
-    
+        public IActionResult AjaxTestAction()
+        {
+            return PartialView("_AjaxTestView");
+        }
     }
 }
---
+++ Ispit_2017_09_11_DotnetCore_grupa2/Ispit_2017_09_11_DotnetCore/Controllers/StavkeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ispit_2017_09_11_DotnetCore.Controllers
+{
+    public class StavkeController : Controller
+    {
+        public IActionResult Index(int odjeljenjeid)
+        {
+
+            return PartialView();
+        }
+    }
+}
