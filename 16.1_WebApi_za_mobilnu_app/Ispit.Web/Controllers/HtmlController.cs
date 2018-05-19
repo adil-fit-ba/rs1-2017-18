c78dafbbc34cebf12ea047df33f2e64bc755ecca
Adil Joldic <adil@edu.fit.ba>
2018-05-19 16:33:16 +0200

    a

---
+++ 16.1_WebApi_za_mobilnu_app/Ispit.Web/Controllers/HtmlController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ispit.Web.Controllers
+{
+    public class HtmlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Camera1()
+        {
+            return View();
+        }
+
+        public IActionResult Gps1()
+        {
+            return View();
+        }
+    }
+}
